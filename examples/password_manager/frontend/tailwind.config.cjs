d2c967739f7ed0db1a5b32247ecd14255efce182
Oleksandr Tkachenko <108659113+altkdf@users.noreply.github.com>
2025-04-01 13:35:47 +0200

    fix some frontend warnings (#58)

--- examples/password_manager/frontend/tailwind.config.cjs
+++ examples/password_manager/frontend/tailwind.config.cjs
@@ -1,7 +1,6 @@
 import daisyui from "daisyui";
-import lineClamp from "@tailwindcss/line-clamp";
 
 export default {
     content: ["./index.html", "./src/**/*.{svelte,js,ts,jsx,tsx}"],
-    plugins: [daisyui, lineClamp],
+    plugins: [daisyui],
 };
--- examples/password_manager/frontend/vite.config.js
+++ examples/password_manager/frontend/vite.config.js
@@ -24,11 +24,11 @@ export default defineConfig({
     }
   },
   build: {
+    sourcemap: true,
     rollupOptions: {
       output: {
         inlineDynamicImports: true,
       },
-      sourcemap: true,
     },
   },
   root: "./",
